247253f73f4748b989851e1f90427e491564a9b1
Michael James Urian <businessmjurian@gmail.com>
2025-06-09 16:58:45 +0800

    feat: enhance Box component with motion support
    - Updated the Box component to extend HTMLMotionProps, enabling animation capabilities using framer-motion.
    - Replaced the static div with a motion.div to allow for animated transitions and improved visual effects.

--- frontend/components/Box.tsx
+++ frontend/components/Box.tsx
@@ -1,23 +1,25 @@
 import { cn } from "@/lib/utils";
 import React from "react";
+import { motion, HTMLMotionProps } from "framer-motion";
 
-interface BoxProps {
+interface BoxProps extends HTMLMotionProps<"div"> {
   children: React.ReactNode;
   className?: string;
   style?: React.CSSProperties;
 }
 
-const Box = ({ children, className, style }: BoxProps) => {
+const Box = ({ children, className, style, ...props }: BoxProps) => {
   return (
-    <div
+    <motion.div
       className={cn(
         "bg-accent rounded-md flex items-center justify-center border border-border",
         className
       )}
       style={style}
+      {...props}
     >
       {children}
-    </div>
+    </motion.div>
   );
 };
 
