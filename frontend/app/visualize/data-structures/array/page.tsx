b1e31fdb13502e790555c6efa177ce82b31896f8
ajurian <adolfjamesurian@gmail.com>
2025-06-13 17:16:43 +0800

    feat: showing random numbers for push and unshift

--- frontend/app/visualize/data-structures/array/page.tsx
+++ frontend/app/visualize/data-structures/array/page.tsx
@@ -3,6 +3,7 @@
 import Array from "@/components/array/Array";
 import { useArray } from "@/components/array/ArrayContext";
 import { Button } from "@/components/ui/button";
+import { getRandomNumber } from "@/lib/utils";
 
 export default function ArrayComponent() {
   const {
@@ -22,7 +23,10 @@ export default function ArrayComponent() {
       </div>
       <Array />
       <div className="flex flex-row gap-2">
-        <Button onClick={() => pushWithAnimation(2)} disabled={isAnimating}>
+        <Button
+          onClick={() => pushWithAnimation(getRandomNumber(0, 100))}
+          disabled={isAnimating}
+        >
           Push
         </Button>
         <Button
@@ -37,7 +41,10 @@ export default function ArrayComponent() {
         >
           Shift
         </Button>
-        <Button onClick={() => unshiftWithAnimation(1)} disabled={isAnimating}>
+        <Button
+          onClick={() => unshiftWithAnimation(getRandomNumber(0, 100))}
+          disabled={isAnimating}
+        >
           Unshift
         </Button>
         <Button onClick={() => bubbleSort()} disabled={isAnimating}>
--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,10 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function getRandomNumber(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
